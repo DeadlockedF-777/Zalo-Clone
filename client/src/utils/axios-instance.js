2467c3a911f0f3aa88155102277e0a5ffc227bc0
DeadlockedF-777 <solar.transxl@gmail.com>
2025-05-07 01:06:03 +0700

    Update axios-instance.js

--- client/src/utils/axios-instance.js
+++ client/src/utils/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: 'http://localhost:5000',
+	baseURL: 'https://https://zalo-clone.onrender.com',
 	withCredentials: true,
 });
 
